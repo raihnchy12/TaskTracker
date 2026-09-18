b07ba9e06961acc099dc44b7a85dd2a82747a2d6
Raihan NUrcahyo <rhinchay12@gmail.com>
2026-09-18 10:57:24 +0700

    fix: import db connection in server.js

--- task-tracker-backend/server.js
+++ task-tracker-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+require('./db');
 
 const authRoutes = require('./routes/auth');
 const projectRoutes = require('./routes/projects');
